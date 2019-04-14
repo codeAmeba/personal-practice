6dd2e01f80f5cfa984456c661a42bcf53d7b93f6
jeongsooyoung <code.ameba@gmail.com>
2019-04-14 19:43:41 +0900

    hello coding upload

---
+++ hello-coding/ComparisonOperators/ComparisonOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("10 > 10 은 ");
+            Console.WriteLine(10 > 10);
+
+            Console.Write("10 >= 10 은 ");
+            Console.WriteLine(10 >= 10);
+
+            Console.Write("5 < 10 은 ");
+            Console.WriteLine(5 < 10);
+
+            Console.Write("5 <= 10 은 ");
+            Console.WriteLine(5 <= 10);
+
+            Console.Write("5 == 4 는 ");
+            Console.WriteLine(5 == 4);
+
+            Console.Write("5 != 4 는 ");
+            Console.WriteLine(5 != 4);
+        }
+    }
+}
---
+++ hello-coding/test1/test1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coinPoket = "주머니 속의 동전: ";
+            Console.Write(coinPoket);
+            int coinA = 10;
+            Console.WriteLine(coinA);
+
+            string coinAram = "아람이가 가져간 동전: ";
+            Console.Write(coinAram);
+            int coinB = coinA / 2 - 1;
+            Console.WriteLine(coinB);
+
+            string coinUram = "우람이가 가져간 동전: ";
+            Console.Write(coinUram);
+            int coinC = coinA - coinB;
+            coinC = coinC / 2 + 2;
+            Console.WriteLine(coinC);
+
+            string coinPoketFinal = "주머니에 남은 동전: ";
+            Console.Write(coinPoketFinal);
+            int coinD = coinA - coinB - coinC;
+            Console.WriteLine(coinD);
+
+
+            
+
+
+       
+
+        }
+    }
+}
